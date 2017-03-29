86bc4310429abbe658aacf9c0cebaeea4f1ce274
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2017-03-30 00:46:27 +0300

    Server side provides json model for navigation

---
+++ appengine/src/main/java/spacecolony/ContentServlet.java
@@ -0,0 +1,26 @@
+package spacecolony;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class ContentServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    InputStream stream = getServletContext().getResourceAsStream("/assets/content" + request.getPathInfo());
+    byte[] buffer = new byte[10240];
+
+    OutputStream output = response.getOutputStream();
+    for (int length = 0; (length = stream.read(buffer)) > 0;) {
+      output.write(buffer, 0, length);
+    }
+    output.close();
+  }
+
+}
