57934283ad0e1a1aa14205d9b7915c494e560278
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2017-03-31 22:17:47 +0300

    Navigation changes url in address bar

--- nodejs/app/components/Navigation.js
+++ nodejs/app/components/Navigation.js
@@ -15,6 +15,11 @@ export default class Navigation extends React.Component {
     window.spacecolony = {
       navigateTo: this.navigateTo.bind(this)
     };
+    window.onpopstate = function(e) {
+      if (e.state) {
+        this.setState(e.state);
+      }
+    }.bind(this);
 
     this.navigateTo({
       label: 'Home',
@@ -29,33 +34,28 @@ export default class Navigation extends React.Component {
 
   navigateTo(page) {
     if (page) {
-      let path = this.state.path;
-
-      this.state.path = [];
-
-      for (let part in path) {
-        if (path[part].navigation === page.navigation) {
+      let path = [];
+      for (let part in this.state.path) {
+        if (this.state.path[part].navigation === page.navigation) {
           break;
         } else {
-          this.state.path.push(path[part]);
+          path.push(this.state.path[part]);
         }
       }
+      path.push(page);
 
-      this.state.path.push(page);
-      this.setState(this.state);
+      window.$.getJSON('/content/' + page.navigation, function(data) {
+        // this if avoids jumping between pages when one download is slower than others
+        let selection = path[path.length - 1];
+        if (selection.navigation === page.navigation) {
+          this.state.path = path;
+          this.state.data = data;
+          this.setState(this.state);
 
-      this.downloadContent(page);
+          window.history.pushState(this.state, null, page.navigation);
+        }
+      }.bind(this));
     }
-  }
-
-  downloadContent(page) {
-    window.$.getJSON('/content/' + page.navigation, function(data) {
-      // this if avoids jumping between pages when one download is slower than others
-      if (this.selection().navigation === page.navigation) {
-        this.state.data = data;
-        this.setState(this.state);
-      }
-    }.bind(this));
   }
 
   render() {
